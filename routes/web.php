416bf5b271285238f00024971bb0fe66d221c0a2
Rohan <rohan.patil@wwindia.com>
2019-12-16 13:26:10 +0530

    added route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/',function(){
+    return view('website.home');
+});
+
+Route::get('/about',function(){
+    return view('website.about');
+});
+
